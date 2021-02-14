8edaee6e2ec5a3ed2965135b4a32d7a6bd8fb354
Nicholas Pignatelli <nickpignatelli@icloud.com>
2021-02-14 12:05:10 -0500

    moved portfolio item content into parent... still be a better spot for it. Why should child component content live in parent component's <script>?

--- src/components/portfolioItem.svelte
+++ src/components/portfolioItem.svelte
@@ -1,39 +1,38 @@
 <script>
+    export let name = "";
+    export let footerDescription = "";
+    export let summary = ``;
+    export let tags = [""];
 </script>
 
 <section>
-    <h2>nicks.rodeo</h2>
-    <p class="footer"><i>personal website and portfolio</i></p>
+    <h2>{name}</h2>
+    <p class="footer"><i>{footerDescription}</i></p>
 
-    <p>
-        If the internet is a Wild Wild West, and a rodeo is a <strong
-            >demonstration of cowboy skills...</strong
-        ><br />then can't my personal website be a <code>.rodeo</code>
-        domain?
-        <br /><br />
-        Whether you find the branding clever or silly, you can checkout the repo
-        below!
-    </p>
+    <p>{@html summary}</p>
 
     <div class="tags">
-        <p class="tag">svelte</p>
-        <p class="tag">sapper</p>
-        <p class="tag">node</p>
-        <p class="tag">html</p>
-        <p class="tag">css</p>
-        <p class="tag">js</p>
-        <p class="tag">rollup</p>
-        <p class="tag">npm</p>
+        {#each tags as tag}
+            <p class="tag">{tag}</p>
+        {/each}
     </div>
-    <div>&nbsp;</div>
 </section>
 
 <style>
-    h1 {
-        text-align: center;
-    }
     h2 {
         margin-bottom: 0.618em;
+    }
+    section {
+        padding: 2.4em 3.2em;
+        border: 8px solid #f8f8ff;
+        border-radius: 2.4em;
+        max-width: min-content;
+        margin-bottom: 3.2em;
+    }
+    p.footer {
+        font-size: 0.718em;
+        letter-spacing: 0.036em;
+        margin-bottom: 2.618em;
     }
     p.tag {
         font-size: 0.82em;
@@ -45,24 +44,12 @@
         display: inline;
         margin: 0.2em 0.32em;
     }
-    p.footer {
-        font-size: 0.718em;
-        letter-spacing: 0.036em;
-        margin-bottom: 2.618em;
-    }
     .tags {
         display: flex;
         flex-direction: row;
         overflow-x: hidden;
         text-shadow: 0 0 4px 14px #f8f8ff;
         margin-left: -0.4em;
-    }
-
-    section {
-        padding: 2.4em 3.2em;
-        border: 8px solid #f8f8ff;
-        border-radius: 2.4em;
-        width: min-content;
-        margin-bottom: 3.2em;
+        max-width: 24em;
     }
 </style>
--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -1,17 +1,55 @@
 <script>
 	import PortfolioItem from "../components/portfolioItem.svelte";
+
+	const portfolioItems = [
+		{
+			id: 1,
+			name: "nicks.rodeo",
+			summary: `If the internet is a Wild Wild West, and a rodeo is a <strong>demonstration of cowboy skills...</strong><br />then couldn't my personal website be a <code>.rodeo</code> domain?<br /><br />Whether you find the branding clever or silly, you can checkout the repo <a href="https://github.com/nicholaspignatelli/nicks.rodeo" target="_blank">here</a> !`,
+			footerDescription: "personal website and portfolio",
+			tags: ["svelte", "sapper", "node", "html", "css", "js"],
+		},
+		{
+			id: 2,
+			name: "mern-todo-app",
+			summary: `A simple web app that allows for the creation, reading, updating, and deletion of a To Do list and it's tasks.`,
+			footerDescription: "MERN Stack CRUD App",
+			tags: ["react.js", "mongodb", "node", "express", "axios", "js"],
+		},
+		{
+			id: 3,
+			name: "virtual-keyboard",
+			summary: `Software peripherals from the land of vanilla JS.`,
+			footerDescription: "qwerty keyboard for the browser",
+			tags: ["svelte", "sapper", "node", "html", "css", "js"],
+		},
+		{
+			id: 4,
+			name: "virtual-keyboard",
+			summary: `Software peripherals from the land of vanilla JS.`,
+			footerDescription: "qwerty keyboard for the browser",
+			tags: ["svelte", "sapper", "node", "html", "css", "js"],
+		},
+		{
+			id: 5,
+			name: "You're a Huge Tank",
+			summary: `Software peripherals from the land of vanilla JS.`,
+			footerDescription: "buddy",
+			tags: ["svelte", "sapper", "node", "html", "css", "js"],
+		},
+	];
 </script>
 
 <svelte:head>
-	<title>nicks.rodoe/portfolio</title>
+	<title>{"nicks.rodeo"}/portfolio</title>
 </svelte:head>
 
 <h1>/portfolio&nbsp;&nbsp;🧑‍💻</h1>
 
 <div class="portfolio">
-	<PortfolioItem />
-	<PortfolioItem />
-	<PortfolioItem /><PortfolioItem /><PortfolioItem /><PortfolioItem />
+	{#each portfolioItems as { name, summary, footerDescription, tags }}
+		<PortfolioItem {name} {summary} {footerDescription} {tags} />
+	{/each}
 </div>
 
 <style>
@@ -20,6 +58,7 @@
 		flex-direction: row;
 		flex-wrap: wrap;
 		justify-content: space-evenly;
+		margin: auto;
 	}
 	h1 {
 		text-align: center;
